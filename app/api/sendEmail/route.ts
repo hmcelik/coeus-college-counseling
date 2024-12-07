063af8d28df6d3e748c6b9438d4cbd4963b8aefa
hmcelik <huseyinmelihcelik@gmail.com>
2024-12-07 16:27:46 +0100

    api fix test

--- app/api/sendEmail/route.ts
+++ app/api/sendEmail/route.ts
@@ -38,6 +38,6 @@ export async function POST(req: Request) {
     return NextResponse.json({ message: 'Email sent successfully' });
   } catch (error) {
     console.error('Error sending email:', error);
-    return NextResponse.json({ message: 'Failed to send email' }, { status: 500 });
-  }
+    return NextResponse.json({ message: 'Failed to send email', error: (error as any).message }, { status: 500 });
+  }  
 }
